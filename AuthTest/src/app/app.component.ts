2a4c13d332f52d7dbfde489e36ba3b9723491717
Daniel Lim <hbt@markhamboards.com>
2017-01-03 13:53:47 -0500

    new str

--- AuthTest/src/app/app.component.ts
+++ AuthTest/src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders } from 'angularfire2';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  isAuth = false;
+  authColor = 'warn';
+  user = {};
+
+  constructor(
+    public af: AngularFire
+  ) {
+    this.af.auth.subscribe(
+      user => this._changeState(user),
+      error => console.trace(error)
+    );
+  }
+
+  login(from: string) {
+    this.af.auth.login({
+      provider: this._getProvider(from)
+    });
+  }
+  logout() {
+    this.af.auth.logout();
+  }
+
+  private _changeState(user: any = null) {
+    if(user) {
+      this.isAuth = true;
+      this.authColor = 'primary';
+      this.user = this._getUserInfo(user)
+    }
+    else {
+      this.isAuth = false;
+      this.authColor = 'warn';
+      this.user = {};
+    }
+  }
+
+  private _getUserInfo(user: any): any {
+    if(!user) {
+      return {};
+    }
+    let data = user.auth.providerData[0];
+    return {
+      name: data.displayName,
+      avatar: data.photoURL,
+      email: data.email,
+      provider: data.providerId
+    };
+  }
+
+  private _getProvider(from: string) {
+    switch(from){
+      case 'twitter': return AuthProviders.Twitter;
+      case 'facebook': return AuthProviders.Facebook;
+      case 'github': return AuthProviders.Github;
+      case 'google': return AuthProviders.Google;
+    }
+  }
 }
