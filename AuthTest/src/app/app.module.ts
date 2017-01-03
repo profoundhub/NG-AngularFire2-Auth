762d05dc0893117a2a4d2f39c6925d7a6a719f3b
Daniel Lim <hbt@markhamboards.com>
2017-01-03 13:50:24 -0500

    update pack and app mod ts
    update pack and app mod ts

--- AuthTest/src/app/app.module.ts
+++ AuthTest/src/app/app.module.ts
@@ -1,9 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+
+// MATERIAL DESIGN MODULES
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdCardModule } from '@angular2-material/card';
+
+export let MD_MODULES: any = [
+  MdToolbarModule,
+  MdButtonModule,
+  MdCardModule
+];
 
 @NgModule({
   declarations: [
